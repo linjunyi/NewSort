3746223e97976225e748c1481f6b9c9faf985832
林君毅 <522526435@qq.com>
2015-07-20 03:02:42 +0800

    Initial Commit

---
+++ NewSort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewSort
+//
+//  Created by lin on 15/7/20.
+//  Copyright (c) 2015年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewSort/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewSort
+//
+//  Created by lin on 15/7/20.
+//  Copyright (c) 2015年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
